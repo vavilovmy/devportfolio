a7adafa9f2a54893b464051933541f1a43ea3710
Mikhail <justfive74@gmail.com>
2025-03-06 19:07:09 +0500

    commit message

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Портфолио vavilovmy",
+  description: "Современное минималистичное портфолио разработчика",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/camel.svg" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "Обо мне", link: "#about" },
+  { name: "Проекты", link: "#projects" },
+  { name: "Отзывы", link: "#testimonials" },
+  { name: "Контакты", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Я ценю командное взаимодействие и открытую коммуникацию ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Я очень гибок в плане временных зон",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Мой стек",
+    description: "Я постоянно развиваюсь",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Отношусь к разработке как к искусству",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Сейчас работаю над созданием блога",
+    description: "Микро-инсайд",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Хотите начать крутой проект вместе?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Chillyabinsk",
+    des: "Один из первых коммерческих проектов и огромная гордость",
+    img: "/chill.png",
+    iconLists: ["/tilda.svg", "/html-5.png","css-3.png", "js.png"],
+    link: "https://www.chillyabinsk.ru",
+  },
+  {
+    id: 2,
+    title: "Портфолио разработчика",
+    des: "Вы сейчас тут, нажмите для перехода на гитхаб",
+    img: "/b1.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/re.svg"],
+    link: "https://github.com/vavilovmy/devportfolio",
+  },
+  {
+    id: 3,
+    title: "Работа кипит",
+    des: "Все апдейты на моем гитхабе!",
+    img: "/github.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/re.svg"],
+    link: "https://github.com/vavilovmy",
+  },
+  {
+    id: 4,
+    title: "Работа кипит",
+    des: "Все апдейты на моем гитхабе!",
+    img: "/github.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/re.svg"],
+    link: "https://github.com/vavilovmy",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Работать с этим разработчиком — настоящее удовольствие! Видна страсть к чистоте кода: компоненты на React/TypeScript продуманы, стили с Tailwind и SCSS — аккуратные и масштабируемые. Next.js использует грамотно, например, для SSR или оптимизации загрузки. Ценю умение слушать фидбэк и быстро вносить правки. Даже сложные задачи, вроде интеграции с Node.js или тонкой адаптивной верстки, выполняет качественно и в срок. Однозначно рекомендую как ответственного и талантливого специалиста!",
+    name: "DeepSeek R1",
+    title: "Умный нейро-кит из Китая",
+  },
+  {
+    quote:
+      "Работать с этим разработчиком — настоящее удовольствие! Видна страсть к чистоте кода: компоненты на React/TypeScript продуманы, стили с Tailwind и SCSS — аккуратные и масштабируемые. Next.js использует грамотно, например, для SSR или оптимизации загрузки. Ценю умение слушать фидбэк и быстро вносить правки. Даже сложные задачи, вроде интеграции с Node.js или тонкой адаптивной верстки, выполняет качественно и в срок. Однозначно рекомендую как ответственного и талантливого специалиста!",
+    name: "DeepSeek R1",
+    title: "Умный нейро-кит из Китая",
+  },
+  {
+    quote:
+      "Работать с этим разработчиком — настоящее удовольствие! Видна страсть к чистоте кода: компоненты на React/TypeScript продуманы, стили с Tailwind и SCSS — аккуратные и масштабируемые. Next.js использует грамотно, например, для SSR или оптимизации загрузки. Ценю умение слушать фидбэк и быстро вносить правки. Даже сложные задачи, вроде интеграции с Node.js или тонкой адаптивной верстки, выполняет качественно и в срок. Однозначно рекомендую как ответственного и талантливого специалиста!",
+    name: "DeepSeek R1",
+    title: "Умный нейро-кит из Китая",
+  },
+  {
+    quote:
+      "Работать с этим разработчиком — настоящее удовольствие! Видна страсть к чистоте кода: компоненты на React/TypeScript продуманы, стили с Tailwind и SCSS — аккуратные и масштабируемые. Next.js использует грамотно, например, для SSR или оптимизации загрузки. Ценю умение слушать фидбэк и быстро вносить правки. Даже сложные задачи, вроде интеграции с Node.js или тонкой адаптивной верстки, выполняет качественно и в срок. Однозначно рекомендую как ответственного и талантливого специалиста!",
+    name: "DeepSeek R1",
+    title: "Умный нейро-кит из Китая",
+  },
+  {
+    quote:
+      "Работать с этим разработчиком — настоящее удовольствие! Видна страсть к чистоте кода: компоненты на React/TypeScript продуманы, стили с Tailwind и SCSS — аккуратные и масштабируемые. Next.js использует грамотно, например, для SSR или оптимизации загрузки. Ценю умение слушать фидбэк и быстро вносить правки. Даже сложные задачи, вроде интеграции с Node.js или тонкой адаптивной верстки, выполняет качественно и в срок. Однозначно рекомендую как ответственного и талантливого специалиста!",
+    name: "DeepSeek R1",
+    title: "Умный нейро-кит из Китая",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Chillyabinsk project owner",
+    desc: "Разработал и запустил первый Чиллябинский бренд - chillyabinsk.ru",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web-development",
+    desc: "Имел опыт коммерческой разработки сайтов для проектов студии Пономарёва, один из них - интернет магазин для бренда одежды chillyabinsk",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Высшее образование",
+    desc: "Имею высшее образование по направлению - Экономика предприятия и управление проектами, работал, как project-owner и экономист",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Работа над пет-проектами",
+    desc: "Не боюсь вызовов и комплексных задач, мечтаю работать в хорошей продуктовой веб-компании",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/vavilovmy"
+  },
+  {
+    id: 2,
+    img: "/telegram-white.svg",
+    href: "https://t.me/vavilovmy"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    href: "#"
+  },
+];
