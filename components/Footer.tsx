12dbbf8a4b61dd49f572c2800b0125cfe6509384
vavilovmy <justfive74@gmail.com>
2025-03-06 20:29:36 +0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Готовы продвинуть <span className="text-purple">ваше</span> вирутальное
+          Готовы продвинуть <span className="text-purple">ваше</span> виртуальное
           присутствие на новый уровень?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
